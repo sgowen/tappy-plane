284a2550da13f41a9fa7a2c59d4ca91ba0b6e67c
Stephen Gowen <dev.sgowen@gmail.com>
2015-08-25 14:04:17 -0400

    Removing commented lines

--- src/platform/3ds/tappy-plane/source/main.cpp
+++ src/platform/3ds/tappy-plane/source/main.cpp
@@ -43,35 +43,8 @@ extern "C"
 
 #define TICKS_PER_SEC (268123480)
 
-//SFX_s* ascendSFX;
-//SFX_s* hitSFX;
-//SFX_s* landSFX;
-//SFX_s* scoreSFX;
-
 int main(int argc, char** argv)
 {
-    //init fs
-//    filesystemInit(argc, argv);
-//
-//    initSound();
-//
-//    char str1[] = "ascend.raw";
-//    char* ascendSFXFn = &str1[0];
-//
-//    char str2[] = "hit.raw";
-//    char* hitSFXFn = &str2[0];
-//
-//    char str3[] = "land.raw";
-//    char* landSFXFn = &str3[0];
-//
-//    char str4[] = "score.raw";
-//    char* scoreSFXFn = &str4[0];
-//
-//    ascendSFX = createSFX(ascendSFXFn, SOUND_FORMAT_16BIT);
-//    hitSFX = createSFX(hitSFXFn, SOUND_FORMAT_16BIT);
-//    landSFX = createSFX(landSFXFn, SOUND_FORMAT_16BIT);
-//    scoreSFX = createSFX(scoreSFXFn, SOUND_FORMAT_16BIT);
-
     DSGameScreen gameScreen = DSGameScreen(400, 240, 320, 240);
 
     u64 lastTick = svcGetSystemTick();
@@ -86,9 +59,6 @@ int main(int argc, char** argv)
         lastTick = newTick;
 
         hidScanInput();
-
-        gameScreen.update(deltaTime);
-        gameScreen.render();
 
         u32 kDown = hidKeysDown();
         if (kDown & KEY_START)
@@ -123,30 +93,33 @@ int main(int argc, char** argv)
         lastTouchPosition.px = touch.px;
         lastTouchPosition.py = touch.py;
 
-//        short soundId;
-//        while ((soundId = gameScreen.getCurrentSoundId()) > 0)
-//        {
-//            switch (soundId)
-//            {
-//            case ASCEND_SOUND:
-//                playSFX(ascendSFX);
-//                break;
-//            case SCORE_SOUND:
-//                playSFX(scoreSFX);
-//                break;
-//            case HIT_SOUND:
-//                playSFX(hitSFX);
-//                break;
-//            case LAND_SOUND:
-//                playSFX(landSFX);
-//                break;
-//            default:
-//                break;
-//            }
-//        }
-    }
+        gameScreen.update(deltaTime);
 
-//    exitSound();
+        gameScreen.render();
+
+        // Handle Sound for this frame
+        short soundId;
+        while ((soundId = gameScreen.getCurrentSoundId()) > 0)
+        {
+            switch (soundId)
+            {
+            case ASCEND_SOUND:
+                // TODO, play sound
+                break;
+            case SCORE_SOUND:
+                // TODO, play sound
+                break;
+            case HIT_SOUND:
+                // TODO, play sound
+                break;
+            case LAND_SOUND:
+                // TODO, play sound
+                break;
+            default:
+                break;
+            }
+        }
+    }
 
     gameScreen.exit();
 
